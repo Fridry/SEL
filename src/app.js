23e9b8ff5445839f56dc7729ed03f7010a7486a1
Fridry <fredwin.ed@gmai.com>
2020-09-13 23:20:52 -0300

    Configuração do CORS

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ const routes = require("./routes");
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Credentials", "*");
+  res.header("Access-Control-Expose-Headers", "x-total-count"); //essta linha habilita o token no header
+  next();
+});
+
 app.use(cors());
 app.use(express.json());
 
